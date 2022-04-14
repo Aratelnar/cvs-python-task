69ce6bfa8ff91ca37d8221c66a68dd339ea1aa73
Aratelnar (Boyakov M) <aratelnar@gmail.com>
2022-04-14 15:45:39 +0500

    fix comments

--- utilities.py
+++ utilities.py
@@ -34,19 +34,14 @@ def repo_find(path=".", required=True):
     if os.path.isdir(os.path.join(path, ".cvs")):
         return structures.Repository(path)
 
-    # If we haven't returned, recurse in parent, if w
     parent = os.path.realpath(os.path.join(path, ".."))
 
     if parent == path:
-        # Bottom case
-        # os.path.join("/", "..") == "/":
-        # If parent==path, then path is root.
         if required:
             raise Exception("No cvs directory.")
         else:
             return None
 
-    # Recursive case
     return repo_find(parent, required)
 
 def repo_default_config():
@@ -65,17 +60,14 @@ def object_read(repo, sha):
     with open (path, "rb") as f:
         raw = zlib.decompress(f.read())
 
-        # Read object type
         x = raw.find(b' ')
         fmt = raw[0:x]
 
-        # Read and validate object size
         y = raw.find(b'\x00', x)
         size = int(raw[x:y].decode("ascii"))
         if size != len(raw)-y-1:
             raise Exception("Malformed object {0}: bad length".format(sha))
 
-        # Pick constructor
         if   fmt==b'commit' : c=structures.Commit
         elif fmt==b'tree'   : c=structures.Tree
         elif fmt==b'tag'    : c=structures.Tag
@@ -83,23 +75,17 @@ def object_read(repo, sha):
         else:
             raise Exception("Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha))
 
-        # Call constructor and return object
         return c(repo, raw[y+1:])
 
 def object_write(obj, actually_write=True):
-    # Serialize object data
     data = obj.serialize()
-    # Add header
     result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
-    # Compute hash
     sha = hashlib.sha1(result).hexdigest()
 
     if actually_write:
-        # Compute path
         path=repo_file(obj.repo, "objects", sha[0:2], sha[2:], mkdir=actually_write)
 
         with open(path, 'wb') as f:
-            # Compress and write
             f.write(zlib.compress(result))
 
     return sha
@@ -110,8 +96,6 @@ def object_find(repo, name, fmt=None, follow=True):
 def object_hash(fd, fmt, repo=None):
     data = fd.read()
 
-    # Choose constructor depending on
-    # object type found in header.
     if   fmt==b'commit' : obj=structures.Commit(repo, data)
     elif fmt==b'tree'   : obj=structures.Tree(repo, data)
     elif fmt==b'tag'    : obj=structures.Tag(repo, data)
@@ -124,42 +108,19 @@ def object_hash(fd, fmt, repo=None):
 def kvlm_parse(raw, start=0, dct=None):
     if not dct:
         dct = collections.OrderedDict()
-        # You CANNOT declare the argument as dct=OrderedDict() or all
-        # call to the functions will endlessly grow the same dict.
 
-    # We search for the next space and the next newline.
     spc = raw.find(b' ', start)
     nl = raw.find(b'\n', start)
 
-    # If space appears before newline, we have a keyword.
-
-    # Base case
-    # =========
-    # If newline appears first (or there's no space at all, in which
-    # case find returns -1), we assume a blank line.  A blank line
-    # means the remainder of the data is the message.
     if (spc < 0) or (nl < spc):
         assert(nl == start)
         dct[b''] = raw[start+1:]
         return dct
 
-    # Recursive case
-    # ==============
-    # we read a key-value pair and recurse for the next.
     key = raw[start:spc]
 
-    # Find the end of the value.  Continuation lines begin with a
-    # space, so we loop until we find a "\n" not followed by a space.
-    end = start
-    while True:
-        end = raw.find(b'\n', end+1)
-        if raw[end+1] != ord(' '): break
+    value = raw[spc+1:nl]
 
-    # Grab the value
-    # Also, drop the leading space on continuation lines
-    value = raw[spc+1:end].replace(b'\n ', b'\n')
-
-    # Don't overwrite existing data contents
     if key in dct:
         if type(dct[key]) == list:
             dct[key].append(value)
@@ -168,42 +129,33 @@ def kvlm_parse(raw, start=0, dct=None):
     else:
         dct[key]=value
 
-    return kvlm_parse(raw, start=end+1, dct=dct)
+    return kvlm_parse(raw, start=nl+1, dct=dct)
 
 def kvlm_serialize(kvlm):
     ret = b''
 
-    # Output fields
     for k in kvlm.keys():
-        # Skip the message itself
         if k == b'': continue
         val = kvlm[k]
-        # Normalize to a list
         if type(val) != list:
             val = [ val ]
 
         for v in val:
             ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
 
-    # Append message
     ret += b'\n' + kvlm[b'']
 
     return ret
 
 def tree_parse_one(raw, start=0):
-    # Find the space terminator of the mode
     x = raw.find(b' ', start)
     assert(x-start == 5 or x-start==6)
 
-    # Read the mode
     mode = raw[start:x]
 
-    # Find the NULL terminator of the path
     y = raw.find(b' ', x+1)
-    # and read the path
     path = raw[x+1:y]
 
-    # Read the SHA and convert to an hex string
     sha = raw[y+1:y+41]
 
     return y+41, structures.TreeRecord(mode, path, sha)
@@ -231,7 +183,6 @@ def tree_serialize(obj):
 def ref_resolve(repo, ref):
     with open(repo_file(repo, ref), 'r') as fp:
         data = fp.read()[:-1]
-        # Drop final \n ^^^^^
     if data.startswith("ref: "):
         return ref_resolve(repo, data[5:])
     else:
