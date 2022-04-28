612ab575c6015aba3a41050c88de10215583cfea
Aratelnar (Boyakov M) <aratelnar@gmail.com>
2022-04-28 11:40:02 +0500

    added anim files

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import difflib
 import filecmp
+import os
 import zlib
 
-
-with open('test/.cvs/objects/63/c1d396edd3726bbcfbf38fdfc90e17612a7b32', 'wb') as f:
-    # print(zlib.decompress(f.read()))
-    f.write(zlib.compress(b'commit 95\x00parent b1461154bf3722923f7a92af83e8adcbb926279b\ntree 90a2b20f8ac2ad289e7f9a8a4ceba2399d68f6bb\n\n'))
+for i in os.walk('test/.cvs/objects/63'):
+    print(i)
